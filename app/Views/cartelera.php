a2fe6a8aa7fdb2c0445f7f56804fb404653bc599
Rogelio Trujillo Martinez <rogeliotrujillo0@gmail.com>
2024-02-16 22:30:49 -0600

    Se agregaron bootstrap y los modal ya mejorados

--- app/Views/cartelera.php
+++ app/Views/cartelera.php
@@ -5,20 +5,7 @@
     <meta charset="UTF-8">
     <title>CINE</title>
     <link rel="stylesheet" href="css/catalogoestilos.css">
-
-
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Archivos JavaScript de Bootstrap (requieren jQuery) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
-    <script>
-        function abrir(){
-            alert("Entre")
-            $('#exampleModal').modal({ show:true });
-        }
-    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
 <body>
@@ -34,7 +21,7 @@
             <div class="info">
                 <div class="pelicula">
                     <img src="<?php echo base_url('resources/Psinaire.jpeg'); ?>" alt="Sin Aire" class="imagenes"
-                        data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+                        data-bs-toggle="modal" onclick="abrir()">
                     <h2>Sin Aire</h2>
                     <p>Descripción de la película...</p>
                     <p>Horarios:</p>
@@ -67,11 +54,32 @@
 
 
 
-    <button onclick="abrir()">Abrir</button>
-        <!-- Button trigger modal -->
-        <div class="modal fade" id="exampleModal" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true"> .... </div>
-    
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+    </button>
 
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 
--- public/javaScript/Scripts.js
+++ public/javaScript/Scripts.js
@@ -1,8 +1,3 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
-
-
-
 function abrir(){
     alert("Entre")
     $('#exampleModal').modal({ show:true });
